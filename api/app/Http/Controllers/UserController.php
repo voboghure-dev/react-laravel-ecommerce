f060368cf2215b2a0a82612a587d77e9c57ae7fc
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-02-14 12:52:06 +0600

    Create controller and api route for register method
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

---
+++ api/app/Http/Controllers/UserController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    //
+    function register()
+    {
+        return 'Hello';
+    }
+}
