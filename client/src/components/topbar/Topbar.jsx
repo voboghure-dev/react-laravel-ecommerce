9a286d6389913296b0873dd970388bd013b6e8d1
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-02-15 13:56:46 +0600

    Finish topbar component
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import './topbar.scss';
+import { NotificationsNone, Language, Settings } from '@mui/icons-material';
+import img from '../../images/profile.jpg';
 
 export default function Topbar() {
   return (
-    <div>Topbar</div>
-  )
+    <div className='topbar'>
+      <div className='topbarWrapper'>
+        <div className='topLeft'>
+          <span className='logo'>ReactJS Admin</span>
+        </div>
+        <div className='topRight'>
+          <div className='topbarIconContainer'>
+            <NotificationsNone />
+            <span className='topIconBadge'>2</span>
+          </div>
+          <div className='topbarIconContainer'>
+            <Language />
+            <span className='topIconBadge'>2</span>
+          </div>
+          <div className='topbarIconContainer'>
+            <Settings />
+          </div>
+          <img src={img} alt='' className='topAvatar' />
+        </div>
+      </div>
+    </div>
+  );
 }
