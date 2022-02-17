b758b6a87b6eb101aff9275c73b6e6bcc6e7a0d4
Tapan Kumer Das <voboghure.dev@gmail.com>
2022-02-17 16:12:47 +0600

    Add user edit form in with MUI Dialog
    Signed-off-by: Tapan Kumer Das <voboghure.dev@gmail.com>

--- client/src/pages/user/UserList.jsx
+++ client/src/pages/user/UserList.jsx
@@ -3,15 +3,55 @@ import { DataGrid } from '@mui/x-data-grid';
 import { Delete, Edit } from '@mui/icons-material';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Alert, Collapse, IconButton, Typography } from '@mui/material';
+import {
+  Alert,
+  Button,
+  Checkbox,
+  Collapse,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControlLabel,
+  Grid,
+  IconButton,
+  TextField,
+  Typography,
+} from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 
 export default function UserList() {
+  // User list
   const [data, setData] = useState([]);
   const [error, setError] = useState('');
   const [message, setMessage] = useState('');
-  const [open, setOpen] = useState(true);
-
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'full_name', headerName: 'Full Name', width: 200 },
+    { field: 'email', headerName: 'Email', width: 200 },
+    { field: 'status', headerName: 'Status', width: 120 },
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 150,
+      renderCell: (params) => {
+        return (
+          <>
+            {/* <Link to={'/user/' + params.row.id}> */}
+            <Edit
+              className='userListEdit'
+              onClick={() => handleDialogOpen(params.row.id)}
+            />
+            {/* </Link> */}
+            <Delete
+              className='userListDelete'
+              onClick={() => handleDelete(params.row.id)}
+            />
+          </>
+        );
+      },
+    },
+  ];
   useEffect(() => {
     async function fetchData() {
       const result = await fetch('http://127.0.0.1:8000/api/list');
@@ -21,6 +61,8 @@ export default function UserList() {
     fetchData();
   }, []);
 
+  // User delete
+  const [alertOpen, setAlertOpen] = useState(true);
   const handleDelete = async (id) => {
     const result = await fetch('http://127.0.0.1:8000/api/delete/' + id, {
       method: 'DELETE',
@@ -39,35 +81,32 @@ export default function UserList() {
       setData(data.filter((item) => item.id !== id));
     });
   };
-  const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
-    { field: 'full_name', headerName: 'Full Name', width: 200 },
-    { field: 'email', headerName: 'Email', width: 200 },
-    { field: 'status', headerName: 'Status', width: 120 },
-    {
-      field: 'action',
-      headerName: 'Action',
-      width: 150,
-      renderCell: (params) => {
-        return (
-          <>
-            <Link to={'/user/' + params.row.id}>
-              <Edit className='userListEdit' />
-            </Link>
-            <Delete
-              className='userListDelete'
-              onClick={() => handleDelete(params.row.id)}
-            />
-          </>
-        );
-      },
-    },
-  ];
+
+  // User update
+  const [dialogOpen, setDialogOpen] = useState(false);
+  const [values, setValues] = useState({});
+  const initialFieldValues = {
+    fullName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    status: '',
+  };
+  const handleInputChange = () => {}
+  const handleCheckboxChange = () => {}
+  const handleFormSubmit = () => {}
+  const handleDialogOpen = (id) => {
+    console.log(id);
+    setDialogOpen(true);
+  };
+  const handleDialogClose = () => {
+    setDialogOpen(false);
+  };
 
   return (
     <div className='userList'>
       {error && (
-        <Collapse in={open}>
+        <Collapse in={alertOpen}>
           <Alert
             severity='error'
             action={
@@ -76,7 +115,7 @@ export default function UserList() {
                 color='inherit'
                 size='small'
                 onClick={() => {
-                  setOpen(false);
+                  setAlertOpen(false);
                 }}
               >
                 <CloseIcon fontSize='inherit' />
@@ -89,7 +128,7 @@ export default function UserList() {
         </Collapse>
       )}
       {message && (
-        <Collapse in={open}>
+        <Collapse in={alertOpen}>
           <Alert
             severity='success'
             action={
@@ -98,7 +137,7 @@ export default function UserList() {
                 color='inherit'
                 size='small'
                 onClick={() => {
-                  setOpen(false);
+                  setAlertOpen(false);
                 }}
               >
                 <CloseIcon fontSize='inherit' />
@@ -122,6 +161,91 @@ export default function UserList() {
         checkboxSelection
         disableSelectionOnClick
       />
+
+      <Dialog open={dialogOpen} onClose={handleDialogClose}>
+        <DialogTitle>Edit User</DialogTitle>
+        <DialogContent>
+          <form autoComplete='off' className='userEntryForm'>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  required
+                  id='fullName'
+                  name='fullName'
+                  label='Full Name'
+                  fullWidth
+                  autoComplete='off'
+                  variant='standard'
+                  value={values.fullName}
+                  onChange={handleInputChange}
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  required
+                  id='email'
+                  name='email'
+                  label='Email address'
+                  fullWidth
+                  autoComplete='off'
+                  variant='standard'
+                  value={values.email}
+                  onChange={handleInputChange}
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  required
+                  id='password'
+                  name='password'
+                  label='Password'
+                  type='password'
+                  fullWidth
+                  autoComplete='off'
+                  variant='standard'
+                  value={values.password}
+                  onChange={handleInputChange}
+                />
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <TextField
+                  required
+                  id='confirmPassword'
+                  name='confirmPassword'
+                  label='Confirm Password'
+                  type='password'
+                  fullWidth
+                  autoComplete='off'
+                  variant='standard'
+                  value={values.confirmPassword}
+                  onChange={handleInputChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      color='secondary'
+                      name='status'
+                      value={values.status}
+                      onChange={handleCheckboxChange}
+                    />
+                  }
+                  label='Active'
+                />
+              </Grid>
+            </Grid>
+          </form>
+        </DialogContent>
+        <DialogActions>
+          <Button variant='contained' onClick={handleDialogClose}>
+            Cancel
+          </Button>
+          <Button variant='contained' onClick={handleFormSubmit}>
+            Update
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
